aeb59ba6fc4933d188903f4eeb7d944023d3c2fc
hdf <hdf@vipmail.hu>
2011-08-09 15:43:55 +0300

    Edited ryppi.py via GitHub

--- ryppi.py
+++ ryppi.py
@@ -72,6 +72,7 @@ class NpmRegistry(object):
             tar = tarfile.open(tmpFilePath)
         packageDir = tar.getmembers()[0].name.split('/')[0] # first entry of tar wil contain destination path
         tar.extractall(path = self.tmpPath)
+        tar.close()
         srcPath = os.path.join(self.tmpPath, packageDir)
         shutil.move(srcPath, destPath)
         self.cleanupDir(self.tmpPath)
